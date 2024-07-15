5807296ecea48d6e603a86f0267e659733093dbc
sankidevil <144487276+sankidevil@users.noreply.github.com>
2024-07-15 10:22:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 from os import getenv
 
-API_ID = int(getenv("API_ID", "6435225")) #optional
-API_HASH = getenv("API_HASH", "") #optional
+API_ID = int(getenv("API_ID", "24543518")) #optional
+API_HASH = getenv("API_HASH", "1b701b5237948e5676351b0ce863ee7c") #optional
 
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-MONGO_URL = getenv("MONGO_URL", "")
-OWNER_ID = int(getenv("OWNER_ID", ""))
+BOT_TOKEN = getenv("BOT_TOKEN", "7272232356:AAFvC0yy5_Tz51pcM1dcOG0p_t8tu_iaezU")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Devilmusic:poLgMY6bWVhpyMGG@cluster0.0bogl8n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+OWNER_ID = int(getenv("OWNER_ID", "6279971449"))
 LOG_GROUP = getenv("LOG_GROUP", "")
 PM_LOGGER = getenv("PM_LOGGER", "")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5368154755").split()))
@@ -15,7 +15,7 @@ BRANCH = getenv("BRANCH", "master") #don't change
 ALIVE_PIC = getenv("ALIVE_PIC", "https://graph.org/file/c716454b7a212f0aaa400.jpg")
 ALIVE_TEXT = getenv("ALIVE_TEXT", "DeViLʙᴏᴛ")
  
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION1 = getenv("STRING_SESSION1", "1BVtsOLQBu5R9cPMmplm21kQiJmiSr1KdrJIDX9VgfidXlP43XJFuPvH7QL9ujP4D-9FAiv3SuWYwb6R-vwByQ-zPjo1XzBDUo-Rf1yA0is05ibX-XwOrQW9SCdFoR6jHeTLwkF5JHM64-bV2dEgybT3ZFim-dXmavvyWJ5eE5vEDB2qMPmt400Pd9yZOFuEPof1dglEaCn9ckK1Hr4UOKvUTo-o9gmabjGJxLwDZDLlxxhawSDUSUnjLqH8dXI5FD1OYh0BtWMxkFXVpmum3Nw7N1GkK_AZM7dwjrxcKOC83zWbONRIEra9PDyBI6k0wfkXSRY6_gzymfjupDBC8_eaN_9StnFs=")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
